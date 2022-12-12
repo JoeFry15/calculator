7a2f27f57f957be6929847e49586d95749a607e7
Joe Fry <joefry@live.co.uk>
2022-12-12 12:42:00 +0000

    Add a simple multiplier

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator!");
+            Console.Write("What is the first number? ");
+            string? firstNumber = Console.ReadLine();
+            Console.Write("What is the second number? ");
+            string? secondNumber = Console.ReadLine();
+            int answer = int.Parse(firstNumber) * int.Parse(secondNumber);
+            Console.WriteLine($"Multiplying these two numbers gives {answer}.");
+        }
+    }
+}
